39d5e70fd1d848c0e05c2bab18134e0926219dff
JTraining <37625000+JTraining@users.noreply.github.com>
2018-03-22 11:51:32 +0300

    Import app from previous project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+//import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+//
+//import Form from './components/Form';
+//
+//class App extends Component {
+//  render() {
+//    return (
+//      <div className="App">
+//        <Form />
+//    
+//      </div>
+//    );
+//  }
+//}
+//
+//export default App;
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
 import './App.css';
+import Form from './components/Form';
 
 class App extends Component {
-  render() {
-    return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-      <div>
-        <h1 className="App-title">Welcome !</h1>
-      </div>
+    state = {
+        mounted: true
+    }
 
-    );
-  }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ mounted: false });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <CSSTransitionGroup
+                    transitionName="fade"
+                    transitionAppear={true}
+                    transitionAppearTimeout={5000}
+                    transitionEnter={false}
+                    transitionLeaveTimeout={300}>
+                    {this.state.mounted && <Form onSubmit={this.handleSubmit} />}
+                </CSSTransitionGroup>
+            </div>
+        );
+    }
 }
 
 export default App;
